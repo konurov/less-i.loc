fd367e8191991f8a0371f98380b484cb8f44f83f
konurov <s_konurov@mail.ru>
2024-03-09 10:53:39 +0600

    my

---
+++ 02/index.php
@@ -0,0 +1,100 @@
+<?php 
+error_reporting(-1);
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+    /*if (условие) {
+            код, который нужно выполнить, если условие выполнено;
+    }
+    $x = 15;
+
+    if ($x >= 10) {
+        echo 'Число больше 10';
+    }
+    if(NULL == 0){
+        echo "OK <br>";
+    }
+    if (3) {
+                echo 'Условие выполнено';
+            }
+    var_dump((bool) 0)."<br>";
+    var_dump((bool) '');
+
+    $var = true;
+    var_dump($var);*/
+    
+       /* $light = 'yellow';
+
+        if( $light == 'green' ){ 
+            echo 'We may go';
+        }elseif( $light == 'yellow' ){
+            echo 'We may ready';
+        }elseif( $light == ' yellow' ){
+            echo 'We may ready1';
+        }
+        else{
+            echo 'We must stop';
+        }*/
+       /*
+        $x = 45;
+        if($x%2 == 0){
+            echo "Жуп сан";
+        }else{
+            echo "Так сан";
+        }
+        $x = 5;
+
+        switch ($x) {
+            case 1:
+                echo 'Число равно 1';
+                break;
+            case 2:
+                echo 'Число равно 2';
+                break;
+            default:
+                echo 'Число не равно ни 1, ни 2';
+        }
+
+         $x = 5;
+
+            if ($x == 1) {
+                echo 'Число равно 1';
+            } elseif ($x == 2) {
+                echo 'Число равно 2';
+            } else {
+                echo 'Число не равно ни 1, ни 2';
+            }
+
+            $x = 10;
+
+            if ($x > 10) {
+                echo 'Число больше 10';
+            } elseif ($x == 10) {
+                echo 'Число равно 10';
+            }*/
+            
+
+
+            $y = 10;
+            switch($y){
+                case ($y > 10):
+                    echo 'Число больше 10';
+                    break;
+                case 10:
+                    echo 'Число равно 10';
+                    break;
+                
+            }
+            
+    
+    ?>
+    Условия в PHP  https://php.zone/kurs-php-dlya-nachinayushih/usloviya-v-php
+</body>
+</html>
